ad45806cc44129672d16620420d755790eada028
askurnikov <kurnikov93@inbox.ru>
2021-02-11 22:32:18 +0300

    task 2.1

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
